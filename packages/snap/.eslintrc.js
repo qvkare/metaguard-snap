ee0c893558ec494852af0a06901362747c1b26ee
qvkare <qvkare@gmail.com>
2024-12-26 18:00:00 +0300

    Update ESLint configurations and add Prettier dependencies

--- packages/snap/.eslintrc.js
+++ packages/snap/.eslintrc.js
@@ -1,80 +1,11 @@
 module.exports = {
-  root: true,
-  extends: [
-    '@metamask/eslint-config',
-    '@metamask/eslint-config-nodejs',
-    '@metamask/eslint-config-typescript',
-    '@metamask/eslint-config-jest',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ['../../.eslintrc.js'],
+
   parserOptions: {
-    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint', 'import', 'jest'],
+
   rules: {
     'import/no-extraneous-dependencies': 'off',
-    'import/no-unresolved': 'error',
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'object',
-          'type',
-        ],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-      },
-    ],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        prefer: 'type-imports',
-        disallowTypeAnnotations: true,
-      },
-    ],
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-shadow': ['error'],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'jest/expect-expect': 'off',
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
-    'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error',
   },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: './tsconfig.json',
-      },
-    },
-  },
-  overrides: [
-    {
-      files: ['**/*.test.ts', '**/*.test.tsx'],
-      rules: {
-        '@typescript-eslint/no-non-null-assertion': 'off',
-      },
-    },
-  ],
 };
