49b2c6bc761e0f3a478ee758e6e102debf61a392
qvkare <qvkare@gmail.com>
2024-12-26 16:15:21 +0300

    Fix project configuration and dependencies

--- jest.config.js
+++ jest.config.js
@@ -4,20 +4,29 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js'
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
     '!src/index.tsx',
-    '!src/serviceWorker.ts',
+    '!src/serviceWorker.ts'
   ],
   coverageThreshold: {
     global: {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80,
-    },
+      statements: 80
+    }
   },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  }
 }; 
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MetaGuard Snap</h1>
+        <p>
+          A MetaMask snap for enhanced transaction security analysis.
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App; 
